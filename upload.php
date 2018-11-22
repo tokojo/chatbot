c17c19adaee7367bd7688f67778c7372ee10e4e5
イリス <iris@irisunoMacBook-puro.local>
2018-11-22 22:47:53 +0900

    cert作り直し

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Google\Cloud\Speech\SpeechClient;
 
 //print_r($_FILES); 
-$size = $_FILES['audio_data']['size']; //the size in bytes
+$size = $_FILES['audio_data']['size']; 
 $input = $_FILES['audio_data']['tmp_name'];
 $output = $_FILES['audio_data']['name'].".wav"; 
 move_uploaded_file($input, $output);
