ee3423737269d972b9ac5bbaa9e20596e3b17506
イリス <jusigisi@gmail.com>
2018-11-23 10:35:02 +0900

    testを戻す

--- upload.php
+++ upload.php
@@ -12,25 +12,21 @@ $input = $_FILES['audio_data']['tmp_name'];
 $output = $_FILES['audio_data']['name'] . time() .".wav"; 
 move_uploaded_file($input, $output);
 
-# Your Google Cloud Platform project ID
 $projectId = 'tactile-stack-223313';
-
-# Instantiates a client
 $speech = new SpeechClient([
     'projectId' => $projectId,
     'languageCode' => 'ja-JP',
 ]);
 
 # wav -> flac
-$flac = "hirosi.flac";
-/*
+$flac = "wave.flac";
 $command = "ffmpeg -i $output $flac";
 exec($command, $out, $res);
 if($res) {
 	echo "Error Command: $command, ";
 	return;
 }
-*/
+
 # The audio file's encoding and sample rate
 $options = [
     'encoding' => 'FLAC'
