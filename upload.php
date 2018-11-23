edbe413e53eba0cb677174f91308b3f59daf0e59
イリス <jusigisi@gmail.com>
2018-11-23 12:43:42 +0900

    配列だった

--- upload.php
+++ upload.php
@@ -35,7 +35,7 @@ $results = $speech->recognize(fopen($flac, 'r'), $options);
 
 $text = "";
 if( !empty($results) ) {
-   $text = $results->alternatives()[0]['transcript'];
+   $text = $results[0]->alternatives()[0]['transcript'];
 }
 
 if( strpos($text, '公園') !== false){
